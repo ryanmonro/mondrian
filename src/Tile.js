7f2f08fe195eb7c6a242c5fdb1b56d7c6cb804c3
ryanmonro <ryan@ryanmonro.info>
2019-03-14 12:40:17 +1100

    Colour tweaks

--- src/Tile.js
+++ src/Tile.js
@@ -4,8 +4,10 @@ import './Tile.scss';
 const colours = {
   "black": "#212121",
   "red": "#F70F0F",
-  "yellow": "#FCE318",
-  "blue": "#0F7FBF",
+  "yellow": "#FCD318",
+  // "blue": "#0F7FBF",
+  // "blue": "#17105D",
+  "blue": "#344D90",
   "grey": "#A3A3A3",
   "white": "#FFFFFF"
 }
