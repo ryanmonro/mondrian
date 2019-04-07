74af10df74816301f5328e9a9f42ca1bc6af71fa
ryanmonro <ryan@ryanmonro.info>
2019-04-07 17:43:37 +1000

    Changed rests to white - it represents 'space' better

--- src/Tile.js
+++ src/Tile.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './Tile.scss';
 
 const colours = {
-  "black": "#212121",
+  "white": "#FFFFFF",
   "red": "#F70F0F",
   "yellow": "#FCD318",
   "blue": "#344D90",
   "grey": "#A3A3A3",
-  "white": "#FFFFFF"
+  "black": "#212121",
 }
 
 const colour = (index) => colours[Object.keys(colours)[index]]
